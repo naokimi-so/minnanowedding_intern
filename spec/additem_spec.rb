16dfafd714672a557efea0544f623a3ef35789a7
NaokiSHIMAI <shimai@planningdev.com>
2016-01-29 16:52:04 +0900

    aaa

--- spec/additem_spec.rb
+++ spec/additem_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe Cart do
     cart = Cart.new
     cart.add(Item.new('Apple',100))
     cart.add(Item.new('Banana',50))
+    cart.add(Item.new('Choco',70))
 
-    expect(cart.total).to eq(150)
+    expect(cart.total).to eq(220)
   end
 end
