b3d9f7bf550c7155fecc59e8022a6b3c02ab5505
NaokiSHIMAI <shimai@planningdev.com>
2016-01-29 17:16:56 +0900

    Database

---
+++ spec/item_database_spec.rb
@@ -0,0 +1,14 @@
+require 'item_database'
+
+RSpec.describe ItemDatabase do
+  it do
+    apple = Item.new('Apple', 100)
+    banana = Item.new('Banana', 50)
+
+    db = ItemDatabase.new
+    db.add(apple)
+    db.add(banana)
+
+    expect(db.find('Apple')).to eq(apple)
+  end
+end
